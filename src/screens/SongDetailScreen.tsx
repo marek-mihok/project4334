aaa2417d5c33b0ecb473ea25023bf7301a5c515d
Marek Mihok <mmihok@Mareks-Mac-mini.local>
2020-07-03 22:54:44 +0200

    code cleaning

--- src/screens/SongDetailScreen.tsx
+++ src/screens/SongDetailScreen.tsx
@@ -10,7 +10,7 @@ import Spacing from '../components/Spacing';
 
 const songTitle = 'Verný Boh';
 //const chordSheet = `{c: intro: 2x}\r\n\r\n[E][c#][A]\r\n\r\n{c: 1:}\r\n\r\n[E] Priblížiť sa k láske, [c#] ktorá seba [A]dáva\r\n[E] priblížiť sa k láske, [c#] ktorá všetko [A]žiada\r\n\r\n{c: prechorus:}\r\n\r\n[f#] Do [E/G#]svojej [H]prítomnosti [E/G#] zahaľ [A]nás\r\n[f#] a v [E/G#]tôni [H]svojich krídel [E/G#] ukry [A]nás\r\n\r\n{c: chorus:}\r\n\r\nVždy byť [E]blíz[H]ko, [c#][A] po tom [E]tú[H]žim[c#][A]\r\ntvoja [E]lás[H]ka [c#][A] je môj [E][c#]ú[H]kryt\r\n\r\n{c: Interlude}\r\n\r\n{c: 1}\r\n\r\n{c: prechorus}\r\n\r\n{c: chorus 2x}\r\n\r\n{c: interlude 2: 2x}\r\n\r\n[A][c#][A][E][H]\r\n\r\n{c: bridge: 6x}\r\n\r\nEmanu[f#]el, Emanu[c#]el\r\nEmanu[f#]el, Emanu[E][H]el\r\n\r\n{c: woah outro: 2x}\r\n\r\n[f#][c#][f#][E][H]`;
-const chordSheetCrdString = `{c: intro}{c: 1:}{column_break}\n[h] Nemôžem [D]prestať [G]myslieť na teba [a]\n[h] nemôžem [D] sa ťa stria[G/F]sť\n[h] Si ako [D]oheň v mojom [G]vnútri, čo páli ma\n[h] p[D]áliš ma[G]\n{c: interlude: 2x}\n[e][G][C]\n{c: 1}{c: prechorus:}\n[e] Si [G]všetko, čo [C]chcem\n[e] Si [G]všetko, čo [C]mám\n[e] A aj keď [D]zdá sa, že sp[E][c#][H]íš\nja tvoje meno zavo[C]lám\nproti búrkam, proti tmám\n{c: chorus: 2x}\nSi [G]verný Boh a [D]verný Kráľ\n[e]dokončíš dielo, [C]ktoré si vo mne začal\n[G]Verný Boh a [D]verný Kráľ\n[e]nevzdáš sa snov, ktoré si [C]so mnou snívať začal\n{c: interlude}\n[G][D][e][C]\n{c: bridge: 4x}\n[G] Ohlásim zo striech\nže [h]ľúbiš mňa a ja ľúbim teba\n[e] Ohlásim zo striech\nže si [C]verný a verný a verný Kráľ\n(si verný a verný Kráľ)\n{c: 1}{c: interlude}{c: 1}{c: prechorus}{c: chorus 2x}{c: interlude}{c: bridge 4x}{c: bridge 2: 4x}\nSi verný [G]Kráľ\nWoa[h]h\nSi verný [e]Kráľ\nWoa[C]h\n{c: outro}`;
+const chordSheetCrdString = `{c: intro}{c: 1:}\n[h] Nemôžem [D]prestať [G]myslieť na teba [a]\n[h] nemôžem [D] sa ťa stria[G/F]sť\n[h] Si ako [D]oheň v mojom [G]vnútri, čo páli ma\n[h] p[D]áliš ma[G]\n{c: interlude: 2x}\n[e][G][C]\n{c: 1}{c: prechorus:}\n[e] Si [G]všetko, čo [C]chcem\n[e] Si [G]všetko, čo [C]mám\n[e] A aj keď [D]zdá sa, že sp[E][c#][H]íš\nja tvoje meno zavo[C]lám\nproti búrkam, proti tmám\n{c: chorus: 2x}\nSi [G]verný Boh a [D]verný Kráľ\n[e]dokončíš dielo, [C]ktoré si vo mne začal\n[G]Verný Boh a [D]verný Kráľ\n[e]nevzdáš sa snov, ktoré si [C]so mnou snívať začal\n{c: interlude}\n[G][D][e][C]\n{c: bridge: 4x}\n[G] Ohlásim zo striech\nže [h]ľúbiš mňa a ja ľúbim teba\n[e] Ohlásim zo striech\nže si [C]verný a verný a verný Kráľ\n(si verný a verný Kráľ)\n{c: 1}{c: interlude}{c: 1}{c: prechorus}{c: chorus 2x}{c: interlude}{c: bridge 4x}{c: bridge 2: 4x}\nSi verný [G]Kráľ\nWoa[h]h\nSi verný [e]Kráľ\nWoa[C]h\n{c: outro}`;
 
 const SongDetailScreen = () => {
   const [chordsVisible, setChordsVisible] = useState(true);
@@ -58,13 +58,13 @@ const SongDetailScreen = () => {
       });
       return captionsParsed;
     }
-    // console.log('row:', row);
+
     // TODO: replace \n's in row with empty row
     const rowStart = row.match(/^\[[^\[\]]+\][ ]/g);
     const rowEnd = row.match(/[ ]\[[^\[\]]+\]$/g);
     const rowMiddle = row?.replace(/^\[[^\[\]]+\][ ]/g, '').replace(/[ ]\[[^\[\]]+\]$/g, '')?.match(/(\[[^\[\]]*\])?[^\[]*/g)?.filter(rowPart => rowPart !== '');
 
-    // console.log('rowMiddle:', rowMiddle);
+
 
     const rowStartParsed = rowStart ? (<View style={chordLyricsStyles.chordsLyricsWrapper}>
     <View style={chordLyricsStyles.chordsWrapper}>
@@ -128,16 +128,15 @@ return(
     );
   });
 
-  // console.log('chordSheet:', chordSheet);
-
   return (
     <>
       <Header title={songTitle} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={{ paddingBottom: 140 }} // TODO: adjust based on button width and device screen
         style={styles.scrollView}>
         <View style={styles.body}>
-          <Text style={styles.songTitle}>{songTitle}</Text>
+          {/* <Text style={styles.songTitle}>{songTitle}</Text */}
           <Spacing size="md" />
           <View style={chordLyricsStyles.lyricsContainer}>
            {chordSheet}
@@ -145,13 +144,13 @@ return(
         </View>
       </ScrollView>
       <View style={styles.buttonWrapper}>
-        <Spacing size="md" />
         <GreenButton
           mode="contained"
           uppercase
           onPress={() => {
             setChordsVisible(!chordsVisible);
-          }}>
+          }}
+          style={styles.button}>
           Akordy
         </GreenButton>
         <Spacing size="sm" />
@@ -160,7 +159,7 @@ return(
           uppercase
           onPress={() => {
             setCaptionsVisible(!captionsVisible);
-          }}>
+          }} style={styles.button}>
           Captions
         </GreenButton>
       </View>
@@ -235,11 +234,13 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   buttonWrapper: {
-    flexGrow: 1,
-    justifyContent: 'flex-end',
-    padding: 16,
-    paddingTop: 20,
+    position: 'absolute', 
+    bottom: 20, 
+    alignSelf: 'center',
   },
+  button: {
+    alignSelf: 'center',
+  }
 });
 
 export default SongDetailScreen;
