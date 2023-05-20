b50bbf2a9f1b6c16b3ce171f79ef902abfdb0ebf
Marek Mihok <marek.mihok@h2o.ai>
2023-05-20 11:31:56 +0200

    reorder nav buttons

--- src/screens/SongDetailScreen.tsx
+++ src/screens/SongDetailScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FunctionComponent } from 'react';
+import React, { useState, FunctionComponent, HTMLAttributes } from 'react';
 import { View, ScrollView, StyleSheet, Image, Animated, Platform, } from 'react-native';
 import { Text, Title, useTheme, IconButton, List } from 'react-native-paper';
 import { Theme } from 'react-native-paper/lib/typescript/src/types';
@@ -18,6 +18,21 @@ import { useAsyncStorage } from '../providers/AsyncStorageProvider';
 //const chordSheet = `{c: intro: 2x}\r\n\r\n[E][c#][A]\r\n\r\n{c: 1:}\r\n\r\n[E] Priblížiť sa k láske, [c#] ktorá seba [A]dáva\r\n[E] priblížiť sa k láske, [c#] ktorá všetko [A]žiada\r\n\r\n{c: prechorus:}\r\n\r\n[f#] Do [E/G#]svojej [H]prítomnosti [E/G#] zahaľ [A]nás\r\n[f#] a v [E/G#]tôni [H]svojich krídel [E/G#] ukry [A]nás\r\n\r\n{c: chorus:}\r\n\r\nVždy byť [E]blíz[H]ko, [c#][A] po tom [E]tú[H]žim[c#][A]\r\ntvoja [E]lás[H]ka [c#][A] je môj [E][c#]ú[H]kryt\r\n\r\n{c: Interlude}\r\n\r\n{c: 1}\r\n\r\n{c: prechorus}\r\n\r\n{c: chorus 2x}\r\n\r\n{c: interlude 2: 2x}\r\n\r\n[A][c#][A][E][H]\r\n\r\n{c: bridge: 6x}\r\n\r\nEmanu[f#]el, Emanu[c#]el\r\nEmanu[f#]el, Emanu[E][H]el\r\n\r\n{c: woah outro: 2x}\r\n\r\n[f#][c#][f#][E][H]`;
 // const chordSheetCrdString = `{c: intro}{c: 1:}\n[h] Nemôžem [D]prestať [G]myslieť na teba [a]\n[h] nemôžem [D] sa ťa stria[G/F]sť\n[h] Si ako [D]oheň v mojom [G]vnútri, čo páli ma\n[h] p[D]áliš ma[G]\n{c: interlude: 2x}\n[e][G][C]\n{c: 1}{c: prechorus:}\n[e] Si [G]všetko, čo [C]chcem\n[e] Si [G]všetko, čo [C]mám\n[e] A aj keď [D]zdá sa, že sp[E][c#][H]íš\nja tvoje meno zavo[C]lám\nproti búrkam, proti tmám\n{c: chorus: 2x}\nSi [G]verný Boh a [D]verný Kráľ\n[e]dokončíš dielo, [C]ktoré si vo mne začal\n[G]Verný Boh a [D]verný Kráľ\n[e]nevzdáš sa snov, ktoré si [C]so mnou snívať začal\n{c: interlude}\n[G][D][e][C]\n{c: bridge: 4x}\n[G][Fmaj7] Ohlásim zo striech\nže [h]ľúbiš mňa a ja ľúbim teba\n[e] Ohlásim zo striech\nže si [C]verný a verný a verný Kráľ\n(si verný a verný Kráľ)\n{c: 1}{c: interlude}{c: 1}{c: prechorus}{c: chorus 2x}{c: interlude}{c: bridge 4x}{c: bridge 2: 4x}\nSi verný [G]Kráľ\nWoa[h]h\nSi verný [e]Kráľ\nWoa[C]h\n{c: outro}`;
 
+// TODO: fix iOS vertical align issue
+// TODO: remove empty spaces left when chords and captions are turned off 
+// TODO: Dno - kurziva font!
+// TODO: zobrazit prazdne riadky, napriklad pred samostatnymi akordami bez textu
+
+// TODO: spotify support
+// TODO: disable scroll animations when text does not overflow screen
+// TODO: horizontal line under multiple chords
+// TODO: check if highlighted active bottom tabs work properly on all devices
+// TODO: finish transposition: transpo indicator, transpo clear
+// TODO: QR Code playlist share
+// TODO: Double tap for showing chords
+// TODO: Optimize performance by not re-computing the song component when showing/hiding chords/captions, but base it just on child component's visibility
+// TODO: handle app when no connection to network 
+
 type Props = {
   navigation: StackNavigationProp<MainParamList, 'SongDetail'>;
   route: RouteProp<MainParamList, 'SongDetail'>;
@@ -25,96 +40,66 @@ type Props = {
 }
 
 const SongDetailScreen: FunctionComponent<Props> = ({ route, navigation }) => {
+  const
+    [chordsVisible, setChordsVisible] = useState(false),
+    [captionsVisible, setCaptionsVisible] = useState(false),
+    insets = useSafeArea(),
+    { songId } = route.params,
+    { state } = useAsyncStorage(),
+    { colors } = useTheme(),
+    { songs, albums, artists } = state,
+    [isInfoExpanded, setIsInfoExpanded] = useState(false),
+    song = songs[songId],
+    album = albums[song.album],
+    artist = artists[album.artist],
+    [textSizes, setTextSizes] = useState([16, 19, 22, 25]),
+    [chordsMajor, setChordsMajor] = useState<string[]>(['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'H'])
 
-  console.log('SongDetailScreen re-rendered.');
+  if (song === undefined) navigation.goBack()
 
-  const [chordsVisible, setChordsVisible] = useState(false);
-  const [captionsVisible, setCaptionsVisible] = useState(false);
-  const insets = useSafeArea();
-  const { songId } = route.params;
-  const { state } = useAsyncStorage();
-  const { colors } = useTheme();
-  const { songs, albums, artists } = state;
-  const [isInfoExpanded, setIsInfoExpanded] = useState(false);
+  const 
+    songTitle = song?.title,
+    chordpro = song?.chordpro,
+    albumTitle = album?.title,
+    artistTitle = artist?.title
 
+  
+  const 
+    metaSpotifyUrl = song?.spotify,
+    metaYoutubeUrl = song?.video,
+    chordSheetCrdString = chordpro?.startsWith('[chordwp]') ? chordpro.substring(9, chordpro.length - 10) : chordpro // TODO: check if all cases are handled
 
-
-  const song = songs[songId];
-  console.log('song:', song);
-  const album = albums[song.album];
-  // console.log('album:', album);
-  const artist = artists[album.artist];
-  // console.log('artist:', artist);
-
-  if (song === undefined) {
-    navigation.goBack();
-  }
-
-
-
-
-  // console.log('song:', songs[songId].chordpro);
-  const songTitle = song?.title;
-  const chordpro = song?.chordpro;
+    console.log(song)
 
   const meta = [
     { decription: song?.tempo + " BPM", value: song?.tempo },
     { decription: song?.key, value: song?.key },
-    { decription: "Capo " + song?.capo, value: song?.capo },
-  ];
+    { decription: "Capo " + song?.capo, value: song?.capo }
+  ]
 
   const metaExtended = [
     { decription: "Referencia", value: song?.bible_ref },
     { decription: "Text", value: song?.text },
     { decription: "Orignál", value: song?.original },
-    { decription: "Preklad", value: song?.translation },
-  ];
-
-  const albumTitle = album?.title;
-  const artistTitle = artist?.title;
-
-  const metaSpotifyUrl = song?.spotify;
-  const metaYoutubeUrl = song?.video;
-  const chordSheetCrdString = chordpro?.startsWith('[chordwp]') ? chordpro.substring(9, chordpro.length - 10) : chordpro; // TODO: check if all cases are handled
-  // console.log('chordSheetCrdString:',chordSheetCrdString);
-
-  const [textSizes, setTextSizes] = useState([16, 19, 22, 25]);
-  // TODO: fix iOS vertical align issue
-  // TODO: remove empty spaces left when chords and captions are turned off 
-  // TODO: Dno - kurziva!
-  // TODO: add metadata
-  // TODO: zobrazit prazdne riadky, napriklad pred samostatnymi akordami bez textu
-
-  // TODO: spotify support
-  // TODO: disable scroll animations when text does not overflow screen
-  // TODO: horizontal line under multiple chords
-  // TODO: check if highlighted active bottom tabs work properly on all devices
-  // TODO: finish transposition: transpo indicator, transpo clear
-  // TODO: QR Code playlist share
-  // TODO: Double tap for showing chords
-  // TODO: Optimize performance by not re-computing the song component when showing/hiding chords/captions, but base it just on child component's visibility
-  // TODO: handle app when no connection to network 
-  const [chordsMajor, setChordsMajor] = useState<string[]>(['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'H']);
+    { decription: "Preklad", value: song?.translation }
+  ]
 
   // animation for bottom tabs
-  const btScrollY = new Animated.Value(0);
-  const btDiffClamp = Animated.diffClamp(btScrollY, 0, 100);
-  const btTranslateY = btDiffClamp.interpolate({ inputRange: [0, 100], outputRange: [0, 100] });
+  const btScrollY = new Animated.Value(0)
+  const btDiffClamp = Animated.diffClamp(btScrollY, 0, 100)
+  const btTranslateY = btDiffClamp.interpolate({ inputRange: [0, 100], outputRange: [0, 100] })
 
   // animation for header
-  const headerScrollY = new Animated.Value(0);
-  const headerDiffClamp = Animated.diffClamp(headerScrollY, 0, 62 + insets.top);
-  const headerTranslateY = headerDiffClamp.interpolate({ inputRange: [0, 62 + insets.top], outputRange: [0, -62 - insets.top] });
+  const headerScrollY = new Animated.Value(0)
+  const headerDiffClamp = Animated.diffClamp(headerScrollY, 0, 62 + insets.top)
+  const headerTranslateY = headerDiffClamp.interpolate({ inputRange: [0, 62 + insets.top], outputRange: [0, -62 - insets.top] })
 
 
   const rotate = (array: any[], times: number, reverse: boolean = false) => {
     array = array.slice();
     while (times--) {
-      if (reverse) {
-        array.unshift(array.pop());
-      } else {
-        array.push(array.shift())
-      }
+      if (reverse) array.unshift(array.pop());
+      else array.push(array.shift()) 
     }
     return array;
   }
@@ -168,8 +153,6 @@ const SongDetailScreen: FunctionComponent<Props> = ({ route, navigation }) => {
   }
 
   const chordSheetRows = chordSheetCrdString.split('\n'); // TODO: what if there is \n\n
-
-  // console.log('chordSheetRows:', chordSheetRows);
 
   const chordSheet = chordSheetRows.map((row, idx) => {
     let captions = row.match(/{[^{}]+}/g)?.filter(row => !row.includes('{ci:')); // TODO: reimplement just to exclude "ci:" not everything with "i"; solve intalic on level of text view https://regex101.com/r/1RHFPt/1    /\{ci:.*?\}/g
@@ -310,6 +293,48 @@ const SongDetailScreen: FunctionComponent<Props> = ({ route, navigation }) => {
     );
   });
 
+  interface SpotifyProps extends HTMLAttributes<HTMLIFrameElement> {
+    [key: string]: any
+  
+    link: string
+    wide?: boolean
+    width?: number | string
+    height?: number | string
+    frameBorder?: number | string
+    allow?: string
+  }
+
+  const Spotify = ({
+    link,
+    style = {},
+    wide = false,
+    width = wide ? "100%" : 300,
+    height = wide ? 80 : 380,
+    frameBorder = 0,
+    allow = "encrypted-media",
+    ...props
+  }: SpotifyProps) => {
+    const url = new URL(link);
+    // https://open.spotify.com/track/1KFxcj3MZrpBGiGA8ZWriv?si=f024c3aa52294aa1
+    return (
+      <iframe
+        title="Spotify Web Player"
+        src={`https://open.spotify.com/embed${url.pathname}`}
+        width={width}
+        height={height}
+        frameBorder={frameBorder}
+        allow={allow}
+        style={{
+          borderRadius: 8,
+          ...style
+        }}
+        {...props}
+      />
+    );
+  };
+
+  // <Spotify wide link="https://open.spotify.com/track/0mpTtYiDqkcOjNZqJLmjsO?si=e116707491c24ffc" />
+
   const DotDelimiter = ({ horizontalPadding }: { horizontalPadding?: number }) => <View style={{ ...{ flexDirection: 'row', alignItems: 'center' }, ...{ paddingHorizontal: horizontalPadding } }}><Text style={{ color: colors.disabled, fontSize: 3 }}>●</Text></View>
 
   return (
@@ -354,7 +379,7 @@ const SongDetailScreen: FunctionComponent<Props> = ({ route, navigation }) => {
             </View>
 
             <TouchableOpacity onPress={() => { setIsInfoExpanded(v => !v) }}>
-              <List.Accordion title="Viac informácií" titleStyle={{ fontWeight: '300' }} expanded={isInfoExpanded}>
+              <List.Accordion title="Viac informácií" titleStyle={{ fontWeight: '400', fontSize: 14 }} expanded={isInfoExpanded}>
                 <View style={{ padding: 16, paddingTop: 0 }}>
                   {metaExtended.map(meta => {
                     if (!meta.value) return;
@@ -375,6 +400,7 @@ const SongDetailScreen: FunctionComponent<Props> = ({ route, navigation }) => {
                 <Text>
               {`SpotifyUrl: ${metaSpotifyUrl}\nYouTubeUrl:${metaYoutubeUrl}`}
                 </Text>
+                <WebView></WebView>
               </View> */}
             <View style={chordLyricsStyles.lyricsContainer}>
               {chordSheet}
@@ -385,10 +411,10 @@ const SongDetailScreen: FunctionComponent<Props> = ({ route, navigation }) => {
       <Animated.View style={{ transform: [{ translateY: btTranslateY }] }}>
         <View style={styles.bottomTabs}>
           <View style={styles.tab} ><InsetShadow containerStyle={styles.tabTouchable} elevation={chordsVisible ? 4 : 0} shadowOpacity={chordsVisible ? 0.4 : 0} shadowRadius={3} ><TouchableHighlight style={styles.tabTouchable} underlayColor={'rgba(255,255,255,0.4)'} onPress={() => { setChordsVisible(!chordsVisible) }}><Image style={{ height: 24, width: 24, }} source={require('../assets/images/icon-guitar-white.png')} /></TouchableHighlight></InsetShadow></View>
-          {/* <View style={styles.tabDivider}></View> */}
-          <View style={styles.tab} ><InsetShadow containerStyle={[styles.tabTouchable, Platform.OS === 'android' && { borderLeftWidth: 1, borderLeftColor: '#44d480', }]} elevation={captionsVisible ? 4 : 0} shadowOpacity={captionsVisible ? 0.4 : 0} shadowRadius={3} ><TouchableHighlight style={styles.tabTouchable} underlayColor={'rgba(255,255,255,0.4)'} onPress={() => { setCaptionsVisible(!captionsVisible) }}><Image style={{ height: 16, width: 36 }} source={require('../assets/images/icon-captions-white.png')} /></TouchableHighlight></InsetShadow></View>
           <View style={styles.tab} ><TouchableHighlight style={styles.tabTouchable} underlayColor={'rgba(255,255,255,0.4)'} onPress={() => { setChordsMajor(rotate(chordsMajor, 1, true)) }} disabled={!chordsVisible}><Title style={[styles.btText, !chordsVisible && { opacity: 0.5 }]}>-1</Title></TouchableHighlight></View>
           <View style={styles.tab} ><TouchableHighlight style={styles.tabTouchable} underlayColor={'rgba(255,255,255,0.4)'} onPress={() => { setChordsMajor(rotate(chordsMajor, 1)) }} disabled={!chordsVisible} ><Title style={[styles.btText, !chordsVisible && { opacity: 0.5 }]}>+1</Title></TouchableHighlight></View>
+          {/* <View style={styles.tabDivider}></View> */}
+          <View style={styles.tab} ><InsetShadow containerStyle={[styles.tabTouchable, Platform.OS === 'android' && { borderLeftWidth: 1, borderLeftColor: '#44d480', }]} elevation={captionsVisible ? 4 : 0} shadowOpacity={captionsVisible ? 0.4 : 0} shadowRadius={3} ><TouchableHighlight style={styles.tabTouchable} underlayColor={'rgba(255,255,255,0.4)'} onPress={() => { setCaptionsVisible(!captionsVisible) }}><Image style={{ height: 16, width: 36 }} source={require('../assets/images/icon-captions-white.png')} /></TouchableHighlight></InsetShadow></View>
           <View style={styles.tab} ><TouchableHighlight style={styles.tabTouchable} underlayColor={'rgba(255,255,255,0.4)'} onPress={() => { setTextSizes(rotate(textSizes, 1)) }}><Title style={styles.btText}>Aa</Title></TouchableHighlight></View>
         </View>
       </Animated.View>
